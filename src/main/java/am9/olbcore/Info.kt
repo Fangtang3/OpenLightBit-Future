1eb007f3613df5185b7804ebf90784e726a4ca32
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2024-06-02 17:43:46 +0800

    Modify | az
    Signed-off-by: Fangtang3 <158278525+Fangtang3@users.noreply.github.com>

--- src/main/java/am9/olbcore/Info.kt
+++ src/main/java/am9/olbcore/Info.kt
@@ -1,6 +1,7 @@
 package am9.olbcore
 
 import am9.openLightBit.Main
+import am9.openLightBit.Core
 
 class Info {
     companion object {
@@ -9,7 +10,7 @@ class Info {
                         "\n运行系统：" + System.getProperty("os.name") + " " + System.getProperty("os.version") +
                         "\n系统架构：" + System.getProperty("os.arch") +
                         "\nJRE版本" + System.getProperty("java.version") +
-                        "\n运行平台：" + Info.platform
+                        "\n运行平台：" + Core.platform
         }
     }
 }
